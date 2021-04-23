cb1e4f2f4d7a3190db10491e68e8e03442fc863a
Ihor <ihor.chernykh@nure.ua>
2021-04-23 13:42:50 +0300

    Finished work with styles

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -11,10 +11,17 @@ class App extends React.Component {
       completed: [],
       failed: [],
     };
+
+    this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   handleClick() {
-    this.state.tasks.push(this.state.task);
+    if (this.state.task.length !== 0) {
+      this.state.tasks.push(this.state.task);
+    } else {
+      this.state.tasks.push('Nothing was added here...');
+    }
     this.setState({tasks: this.state.tasks});
     this.setState({task: ''});
   }
@@ -50,10 +57,10 @@ class App extends React.Component {
                 <path d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.235.235 0 0 1 .02-.022z"/>
               </svg>
             </Button> 
-            <Button type="submit" className="btn btn-danger" handleClick={this.handleFailed.bind(this, index)}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
-                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-                <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+            <Button type="submit" className="btn btn-danger " handleClick={this.handleFailed.bind(this, index)}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-square" viewBox="0 0 16 16">
+                <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
               </svg>
             </Button>
           </div>
@@ -83,8 +90,8 @@ class App extends React.Component {
         <h1 className="header">To-do-list</h1>
         <hr />
         <div className="searchPanel">
-          <input type="text" id="taskName" className="taskName" value={this.state.task} onChange={this.handleChange.bind(this)} />
-          <Button type="submit" className="addBtn" handleClick={this.handleClick.bind(this)}>Add task</Button>
+          <input type="text" id="taskName" className="taskName" value={this.state.task} onChange={this.handleChange} placeholder="Enter your task here..." />
+          <Button type="submit" className="addBtn" handleClick={this.handleClick}>Add task</Button>
         </div>
         <div id="tasks">
           {tasksToDo}
@@ -92,12 +99,12 @@ class App extends React.Component {
       </div>
       <div id="result">
         <div className="completedTasks">
-          <h3 className="task-type">Completed tasks</h3> quantity<span>{this.state.completed.length}</span>
+          <h3 className="task-type">Completed tasks: <span className="quantity">{this.state.completed.length}</span></h3>
             <hr className="hor_line" />
               {completedTasks}
         </div>
         <div class="failedTasks">
-          <h3 className="task-type">Failed tasks</h3>
+          <h3 className="task-type">Failed tasks: <span className="quantity">{this.state.failed.length}</span></h3>
             <hr className="hor_line" />
               {failedTasks}
         </div>
